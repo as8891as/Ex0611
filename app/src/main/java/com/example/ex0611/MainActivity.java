ce665a95a25e9d5d2078e2f18d4513a2e0fa2ec8
as8891as <as8891@bs.amalnet.k12.il>
2025-05-05 22:41:44 +0300

    Manuel change finished

--- app/src/main/java/com/example/ex0611/MainActivity.java
+++ app/src/main/java/com/example/ex0611/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.ex0611;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Switch sw;
+    RadioButton rB1, rB2, rB3, rB4;
+    ConstraintLayout cL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sw = findViewById(R.id.sw);
+        rB1 = findViewById(R.id.rB1);
+        rB2 = findViewById(R.id.rB2);
+        rB3 = findViewById(R.id.rB3);
+        rB4 = findViewById(R.id.rB4);
+        cL = findViewById(R.id.main);
+    }
+
+    public void ManuelChange(View view) {
+        if (!sw.isChecked())
+        {
+            if (rB1.isChecked())
+            {
+                cL.setBackgroundColor(Color.WHITE);
+            }
+            else if (rB2.isChecked())
+            {
+                cL.setBackgroundColor(Color.RED);
+            }
+            else if (rB3.isChecked())
+            {
+                cL.setBackgroundColor(Color.GREEN);
+            }
+            else
+            {
+                cL.setBackgroundColor(Color.BLUE);
+            }
+        }
     }
 }
